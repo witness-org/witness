3168c8ce92aa673d0197f83d5f3e905486d86ec3
Raffael Foidl <raffael.foidl@tuwien.ac.at>
2021-10-25 21:27:51 +0200

    50: Add conversion layer between JSON deserialization and test argument provision in @JsonFileSource

--- server/src/test/java/com/witness/server/util/JsonFileSource.java
+++ server/src/test/java/com/witness/server/util/JsonFileSource.java
@@ -1,5 +1,7 @@
 package com.witness.server.util;
 
+import com.witness.server.util.converters.ArgumentConverter;
+import com.witness.server.util.converters.NoOpArgumentConverter;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -35,4 +37,34 @@ public @interface JsonFileSource {
    *     {@link JsonFileSource} should be passed as a list instead of an array of {@link JsonFileSource#type()} objects to the respective test method.
    */
   boolean arrayToList() default false;
+
+  /**
+   * Provides the option of introducing a "conversion layer" between JSON deserialization and test argument provision. An instance of
+   * {@link ArgumentConverter} may be used to convert the object deserialized from JSON to another type before supplying it to the test.
+   * <p>
+   * For example, {@code ExternalModel} might not be deserializable. Therefore, {@code ExternalModelStub} and {@code ExternalModelStubConverter} were
+   * created for test purposes. A test using the converter along with the stub to get an instance of the target type can be found below.
+   * </p>
+   * <pre>
+   *  &#64;ParameterizedTest
+   *  &#64;JsonFileSources(parameters = {
+   *    &#64;JsonFileSource(value = "ExternalModel1.json", type = ExternalModel.class, stubConverter = ExternalModelStubConverter.class)
+   *  })
+   *  void dtoToEntity(ExternalModel model) {
+   *    // test code
+   *  }
+   * </pre>
+   * <p>
+   * The file {@code ExternalModel1.json} holds a representation of an {@code ExternalModelStub} instance. Upon supplying arguments to the test,
+   * {@link com.witness.server.util.JsonFileArgumentsProvider} deserializes an {@code ExternalModelStub} instance, converts it to an
+   * {@code ExternalModel} instance by invoking {@link ArgumentConverter#toConcreteInstance(Object)} and only then supplies it to the test case.
+   * </p>
+   * <p>
+   * If no custom {@link ArgumentConverter} implementation is specified, {@link NoOpArgumentConverter} is used. It converts nothing and returns the
+   * deserialized instances verbatim.
+   * </p>
+   *
+   * @return the {@link NoOpArgumentConverter} to be used as conversion layer between JSON deserialization and test argument provision
+   */
+  Class<? extends ArgumentConverter<?, ?>> converter() default NoOpArgumentConverter.class;
 }
