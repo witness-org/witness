66aa0b93e566aa3ff07535810ef45e0bef954299
Raffael Foidl <raffael.foidl@tuwien.ac.at>
2021-10-25 21:15:15 +0200

    50: Add tests for FirebaseService

---
+++ server/src/test/java/com/witness/server/util/converters/UserRecordArgumentConverter.java
@@ -0,0 +1,33 @@
+package com.witness.server.util.converters;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.firebase.auth.UserRecord;
+import com.witness.server.util.stubs.UserRecordStub;
+
+/**
+ * Implementation of {@link ArgumentConverter} for {@link UserRecord} and {@link UserRecordStub} instances.
+ */
+public class UserRecordArgumentConverter extends ArgumentConverter<UserRecord, UserRecordStub> {
+  @Override
+  UserRecord toConcreteInstanceInternal(UserRecordStub intermediate) {
+    var record = mock(UserRecord.class);
+    when(record.getUid()).thenReturn(intermediate.getUid());
+    when(record.getDisplayName()).thenReturn(intermediate.getDisplayName());
+    when(record.getEmail()).thenReturn(intermediate.getEmail());
+    when(record.isEmailVerified()).thenReturn(intermediate.isEmailVerified());
+    when(record.getPhotoUrl()).thenReturn(intermediate.getPhotoUrl());
+    return record;
+  }
+
+  @Override
+  public Class<UserRecordStub> intermediateClass() {
+    return UserRecordStub.class;
+  }
+
+  @Override
+  public Class<UserRecord> targetClass() {
+    return UserRecord.class;
+  }
+}
